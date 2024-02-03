11b109cdc0b297dca3c91e5d6fdfc1895fc15c64
arezki33 <153658360+arezki33@users.noreply.github.com>
2024-02-03 10:42:38 +0100

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
 mongoose
-       .connect ('mongodb+srv://'+ process.env.DB_USER_PASS + '@cluster0.rezvrxr.mongodb.net/MailWalker',)
+       .connect ('mongodb+srv://'+ process.env.DB_USER_PASS + '@cluster0.rezvrxr.mongodb.net/MailWalkerV1',)
       .then(() => console.log('Connected to MongoDB'))
       .catch((err) => console.log("failed to connect to MongoDB", err));
